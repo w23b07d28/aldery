1519df00de3525259ab778b8cf203dd8c3a9a125
Jakub Olszewski <57095596+w23b07d28@users.noreply.github.com>
2023-04-10 23:55:28 +0200

    Install.sh from void to fedora

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
-#!/bin/sh
-echo "Install git and clone the VOPI repository to start installation..."
+#!/bin/bash
 
-sudo xbps-install git
-git clone https://github.com/balintkiraly/vopi.git /tmp/vopi
-cd /tmp/vopi || exit
+# Install Git and clone the repository to start installation
+echo "Installing Git and cloning the repository..."
+sudo dnf install git -y || { echo "Failed to install Git."; exit 1; }
+git clone https://github.com/w23b07d28/aldery.git /tmp/aldery || { echo "Failed to clone the repository."; exit 1; }
+cd /tmp/aldery/src || { echo "Failed to change directory."; exit 1; }
 
-bash ./src/main.sh
+# Run the installation script
+echo "Running the installation script..."
+bash ./main.sh || { echo "Failed to run the installation script."; exit 1; }
+
+# Remove the cloned repository from /tmp
+echo "Removing the cloned repository from /tmp..."
+rm -rf /tmp/aldery
