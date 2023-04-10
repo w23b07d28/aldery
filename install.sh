e40a716aa0607099470012f52a2be00b13274308
Jakub Olszewski <57095596+w23b07d28@users.noreply.github.com>
2023-04-11 01:41:48 +0200

    fgggggggggggggggggg

--- install.sh
+++ install.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+# Remove any previous Aldery installation
 rm -rf /tmp/aldery
+
+# Install Git
+echo "Installing Git..."
 sudo dnf install git -y
+
+# Clone the Aldery repository to /tmp/aldery and run the installation script
+echo "Cloning the Aldery repository and starting the installation..."
 git clone https://github.com/w23b07d28/aldery.git /tmp/aldery
 cd /tmp/aldery || exit
 bash ./src/main.sh
+
+# Clean up the temporary directory
+echo "Cleaning up..."
 rm -rf /tmp/aldery
+
+echo "Installation completed successfully."
