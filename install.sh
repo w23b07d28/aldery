71aa8f79e1277e94d8a22c8f6b804a3442ae862d
Jakub Olszewski <57095596+w23b07d28@users.noreply.github.com>
2023-04-11 01:21:51 +0200

    aaa

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-git clone https://github.com/w23b07d28/aldery.git /tmp/aldery
-cd /tmp/aldery || exit
-bash ./src/main.sh
+# Install Git and clone the repository to start installation
+echo "Installing Git and cloning the repository..."
+sudo dnf install git -y || { echo "Failed to install Git."; exit 1; }
+ || { echo "Failed to clone the repository."; exit 1; }
+cd /tmp/aldery/src || { echo "Failed to change directory."; exit 1; }
+
+# Run the installation script
+echo "Running the installation script..."
+bash ./src/main.sh || { echo "Failed to run the installation script."; exit 1; }
+
+# Remove the cloned repository from /tmp
+echo "Removing the cloned repository from /tmp..."
+rm -rf /tmp/aldery
