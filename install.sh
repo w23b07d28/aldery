1d506d050b72b3c2fd4927af031463f895e08184
Jakub Olszewski <57095596+w23b07d28@users.noreply.github.com>
2023-04-11 01:24:08 +0200

    saa

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Install Git and clone the repository to start installation
 echo "Installing Git and cloning the repository..."
 sudo dnf install git -y || { echo "Failed to install Git."; exit 1; }
- || { echo "Failed to clone the repository."; exit 1; }
+git clone https://github.com/w23b07d28/aldery.git /tmp/aldery || { echo "Failed to clone the repository."; exit 1; }
 cd /tmp/aldery/src || { echo "Failed to change directory."; exit 1; }
 
 # Run the installation script
--- src/main.sh
+++ src/main.sh
@@ -1,35 +1,23 @@
 #!/bin/bash
 
-# Set the base directory to the current working directory
+source ./src/modules/yaml.sh
 BASEDIR=$(pwd)
 
-# Source the YAML module
-source ./src/modules/yaml.sh
+echo "Install programs.."
 
-# Print a message indicating that program installation is starting
-echo "Installing programs..."
-
-# Loop through each file in the "programs" directory
-find ./src/programs -type f -print0 | while IFS= read -r -d $'\0' file; do
-  
-  # Get the package name from the file name
+find ./src/programs -type f -print0 | while IFS= read -r -d $'\0' file; 
+do
   package_name=$(basename "$file")
-  
-  # Convert hyphens to underscores in the package name
   base_name=$(echo "$package_name" | tr "-" "_") 
-  
-  # Create variables for the pre-install, install, and post-install scripts
+  create_variables "$file" "$base_name"
+
   preinstall="${base_name}preinstall"
   install="${base_name}install"
   postinstall="${base_name}postinstall"
 
-  # Execute the pre-install script if it exists
-  if [ -n "${!preinstall}" ]; then
-    # shellcheck disable=SC2086
-    eval ${!preinstall}
-  fi
+  # shellcheck disable=SC2086
+  [ -n "${!preinstall}" ] && eval ${!preinstall}
 
-  # Execute the install script if it exists, otherwise install the package using the package manager
   if [ -n "${!install}" ]; then
     # shellcheck disable=SC2086
     eval ${!install}
@@ -37,18 +25,12 @@ find ./src/programs -type f -print0 | while IFS= read -r -d $'\0' file; do
     sudo xbps-install -y "${package_name}"
   fi
 
-  # Execute the post-install script if it exists
-  if [ -n "${!postinstall}" ]; then
-    # shellcheck disable=SC2086
-    eval ${!postinstall}
-  fi
+  # shellcheck disable=SC2086
+  [ -n "${!postinstall}" ] && eval ${!postinstall}
 
-  # Change the working directory back to the base directory
   cd "$BASEDIR" || exit
 done
 
-# Print a message indicating that program installation is complete
-echo "Program installation complete."
 
-# echo "Download dotfiles"
-# git clone --bare https://github.com/w23b07d28/dotfiles.git "$HOME/.dotfiles"
+#cho "Download dotfiles"
+#git clone --bare https://github.com/w23b07d28/dotfiles.git "$HOME/.dotfiles"
