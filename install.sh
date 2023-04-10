82b06da9da722992afcc1f6774e0fa935a1bef8e
Jakub Olszewski <57095596+w23b07d28@users.noreply.github.com>
2023-04-11 01:39:25 +0200

    ggggggggggg

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-Remove any previous Aldery installation
 
+# Remove any previous Aldery installation
 rm -rf /tmp/aldery
-Install Git
 
+# Install Git
 echo "Installing Git..."
 sudo dnf install git -y
-Clone the Aldery repository to /tmp/aldery and run the installation script
 
+# Clone the Aldery repository to /tmp/aldery and run the installation script
 echo "Cloning the Aldery repository and starting the installation..."
 git clone https://github.com/w23b07d28/aldery.git /tmp/aldery
 cd /tmp/aldery || { echo "Error: Could not change to directory /tmp/aldery. Exiting." >&2; exit 1; }
 bash ./src/main.sh
-Clean up the temporary directory
 
+# Clean up the temporary directory
 echo "Cleaning up..."
 rm -rf /tmp/aldery
 
