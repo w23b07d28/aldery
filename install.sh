7c23523d7880610d64aa832ca4788a9e336e3a13
Jakub Olszewski <57095596+w23b07d28@users.noreply.github.com>
2023-04-11 01:29:51 +0200

    ll

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
 #!/bin/sh
 echo "Install git and clone the Aldery repository to start installation..."
+
+rm -rf /tmp/aldery
 
 sudo dnf install git -y
 git clone https://github.com/w23b07d28/aldery.git /tmp/aldery
