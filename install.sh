b96e6320b1b944d2f0a81a0733b45be0c316a3ac
Jakub Olszewski <57095596+w23b07d28@users.noreply.github.com>
2023-04-11 01:38:29 +0200

    aaaaaaaaaa

--- install.sh
+++ install.sh
@@ -1,39 +1,20 @@
 #!/bin/bash
-
-set -e # stop the script on the first error
-
-# function to display an error message and exit the script
-error_exit() {
-  echo "$1" >&2 # display the message on the standard error output
-  exit "${2:-1}" # return the given error code or 1 by default
-}
-
-# function to check if a program is installed and install it if needed
-install_package() {
-  if ! command -v "$1" &> /dev/null; then # if the program is not installed
-    if [ "$#" -eq 2 ]; then # if the package name is provided
-      echo "Installing $2..."
-      sudo dnf install "$2" -y || error_exit "Failed to install $2." "$?" # install the package
-    else # if the package name is not provided
-      error_exit "$1 is not installed and no package name provided."
-    fi
-  fi
-}
-
-install_package git # install git
+Remove any previous Aldery installation
 
 rm -rf /tmp/aldery
+Install Git
 
-# clone the repository and run the installation
+echo "Installing Git..."
+sudo dnf install git -y
+Clone the Aldery repository to /tmp/aldery and run the installation script
+
 echo "Cloning the Aldery repository and starting the installation..."
-if git clone https://github.com/w23b07d28/aldery.git /tmp/aldery; then
-  cd /tmp/aldery/src || error_exit "Cannot enter the Aldery directory."
-  bash main.sh || error_exit "Installation failed."
-else
-  error_exit "Failed to clone the Aldery repository."
-fi
+git clone https://github.com/w23b07d28/aldery.git /tmp/aldery
+cd /tmp/aldery || { echo "Error: Could not change to directory /tmp/aldery. Exiting." >&2; exit 1; }
+bash ./src/main.sh
+Clean up the temporary directory
 
-# remove the repository
+echo "Cleaning up..."
 rm -rf /tmp/aldery
 
 echo "Installation completed successfully."
