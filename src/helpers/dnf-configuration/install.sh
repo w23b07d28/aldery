5de084600882e7301d6f3661b0c952e6260b9c82
Jakub Olszewski <57095596+w23b07d28@users.noreply.github.com>
2023-04-11 15:37:43 +0200

    Update install.sh

--- src/helpers/dnf-configuration/install.sh
+++ src/helpers/dnf-configuration/install.sh
@@ -3,9 +3,6 @@
 # By selecting the fastest mirrors, the package download speed can increase.
 # The fastest mirrors work by selecting the mirror with the lowest latency.
 
-fastestmirror=true
-deltarpm=true
-
 # Set DNF configuration options
 CONF_FILE="/etc/dnf/dnf.conf"
 CONF_OPTS=("max_parallel_downloads=10" "fastestmirror=true" "deltarpm=true")
