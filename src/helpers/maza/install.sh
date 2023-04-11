144aef0af23ae25f9a520c8ec62ee4e18940f27f
Jakub Olszewski <57095596+w23b07d28@users.noreply.github.com>
2023-04-11 15:46:28 +0200

    Update install.sh

--- src/helpers/maza/install.sh
+++ src/helpers/maza/install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-# Install local ad blocker
+# Local ad blocker
 
 curl -o maza https://raw.githubusercontent.com/tanrax/maza-ad-blocking/master/maza
 chmod +x maza
 sudo mv maza /usr/local/bin
 maza update
+sudo maza stop
 sudo maza start
