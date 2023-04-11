16a848061a4b9f2f7f8eb6453c73540d944ba728
Jakub Olszewski <57095596+w23b07d28@users.noreply.github.com>
2023-04-11 03:52:19 +0200

    yyy

--- src/main.sh
+++ src/main.sh
@@ -8,8 +8,8 @@ sudo dnf update -y
 sudo dnf upgrade -y
 
 # Enable RPM Fusion Repository, Flathub repository and Snaps.
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo -y
 sudo dnf install snapd -y
 sudo ln -s /var/lib/snapd/snap /snap
