d439c35524c3619b457b47174942c35a822d9f08
Jakub Olszewski <57095596+w23b07d28@users.noreply.github.com>
2023-04-11 02:00:48 +0200

    ffffffffff

--- src/helpers/set-dnf-configuration-options/install.sh
+++ src/helpers/set-dnf-configuration-options/install.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+# Several methods can enhance the speed of downloading packages in Fedora.
+# By selecting the fastest mirrors, the package download speed can increase.
+# Furthermore, if you have a sufficiently faster internet connection,
+# then the number of parallel downloads can be changed to get faster downloads.
+# The fastest mirrors work by selecting the mirror with the lowest latency.
+# The speed difference may or may not be noticeable.
+
+fastestmirror=true
+deltarpm=true
+
 # Set DNF configuration options
 CONF_FILE="/etc/dnf/dnf.conf"
 CONF_OPTS=("max_parallel_downloads=10" "fastestmirror=true" "deltarpm=true")
