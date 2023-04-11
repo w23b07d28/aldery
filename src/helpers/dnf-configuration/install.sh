4431bc6467444169657f0d536041629e36598874
Jakub Olszewski <57095596+w23b07d28@users.noreply.github.com>
2023-04-11 16:47:28 +0200

    Sed to substitute a line in the file
    Sed to substitute a line in the file if a value for max_parallel_downloads already exists.

--- src/helpers/dnf-configuration/install.sh
+++ src/helpers/dnf-configuration/install.sh
@@ -10,5 +10,7 @@ CONF_OPTS=("max_parallel_downloads=10" "fastestmirror=true" "deltarpm=true")
 for OPT in "${CONF_OPTS[@]}"; do
     if ! grep -q "^${OPT}$" "$CONF_FILE"; then
         echo "$OPT" >> "$CONF_FILE"
+    elif [[ $OPT == max_parallel_downloads=* ]]; then
+        sed -i "s/^max_parallel_downloads=.*/$OPT/" "$CONF_FILE"
     fi
 done
