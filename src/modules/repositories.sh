ade093acc540497b6c9e63c847029368559102f2
Jakub Olszewski <57095596+w23b07d28@users.noreply.github.com>
2023-04-13 13:15:08 +0200

    some updates to updates

--- src/modules/repositories.sh
+++ src/modules/repositories.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 
-# The script enables RPM Fusion Repository, Flathub repository, and Snaps by
-# installing necessary packages and adding required repositories. It also installs
-# npm and globally installs yarn. Each command is executed in sequence in the terminal.
+# RPM Fusion free and nonfree repositories.
+sudo dnf install -y  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf upgrade --refresh
+sudo dnf groupupdate core
+sudo dnf install -y rpmfusion-free-release-tainted
+sudo dnf install -y dnf-plugins-core
 
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
-sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo -y
-sudo dnf install snapd -y
-sudo ln -s /var/lib/snapd/snap /snap
+# Flatpak is installed by default,
+# but one needs to enable the Flathub store.
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak update
+
+# Snap support.
+sudo dnf install -y snapd
+sudo ln -s /var/lib/snapd/snap /snap # for classic snap support
 sudo snap install core
 sudo snap refresh core
-sudo dnf install npm -y
-sudo npm install yarn -g
+sudo snap refresh
+
+#sudo dnf install npm -y
+#sudo npm install yarn -g
