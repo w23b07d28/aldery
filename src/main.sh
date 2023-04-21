2be506ea61d8e1b09c264c8b59e5e977d8ab2b3a
Jakub Olszewski <57095596+w23b07d28@users.noreply.github.com>
2023-04-22 01:23:57 +0200

    Added repo module.

--- src/main.sh
+++ src/main.sh
@@ -1,38 +1,6 @@
 #!/usr/bin/env bash
 
-#source ./src/modules/repositories.sh
-
-# RPM Fusion free and nonfree repositories
-sudo dnf install --color=always -qy https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install --color=always -qy https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf upgrade --refresh --color=always -qy
-sudo dnf groupupdate core --color=always -qy
-sudo dnf install --color=always -qy rpmfusion-free-release-tainted
-sudo dnf install --color=always -qy dnf-plugins-core
-
-# Flatpak is installed by default
-# but one needs to enable the Flathub store
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak update
-
-# Snap support
-sudo dnf install --color=always -y snapd
-sudo ln -s /var/lib/snapd/snap /snap # for classic snap support
-sudo snap install core
-sudo snap refresh core
-sudo snap refresh
-
-# Update system
-sudo dnf update --color=always -qy
-sudo dnf upgrade --color=always -qy
-sudo dnf upgrade --color=always --refresh -qy
-sudo dnf check --color=always -qy
-sudo dnf autoremove --color=always -qy
-sudo fwupdmgr get-devices -y
-sudo fwupdmgr refresh --force -y
-sudo fwupdmgr get-updates -y
-sudo fwupdmgr update -y
-
+source ./src/modules/repositories.sh
 source ./src/modules/yaml.sh
 BASEDIR=$(pwd)
 
