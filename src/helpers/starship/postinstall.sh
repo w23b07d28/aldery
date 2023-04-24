3663329372bee253ff0d59d85c8e7c1b8b31f1e9
Jakub Olszewski <57095596+w23b07d28@users.noreply.github.com>
2023-04-24 07:12:41 +0200

    Starship autoinstall.

---
+++ src/helpers/starship/postinstall.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Check if Bash is installed
+if ! command -v bash &> /dev/null; then
+    echo "Bash is not installed. Please install Bash before running this script."
+    exit
+fi
+
+# Add or move the starship init bash command to the end of ~/.bashrc
+if ! grep -q 'eval "\$\(starship init bash\)"' "$HOME/.bashrc" 2>/dev/null; then
+    echo 'eval "$(starship init bash)"' >> "$HOME/.bashrc"
+fi
+
+# Check if Fish shell is installed
+if ! command -v fish &> /dev/null; then
+    echo "Fish shell is not installed. Skipping Fish shell configuration."
+    exit
+fi
+
+# Set the XDG_CONFIG_HOME environment variable if it is not set
+: ${XDG_CONFIG_HOME:="$HOME/.config"}
+
+# Add or move the starship init bash command to the end of $XDG_CONFIG_HOME/fish/config.fish
+if ! grep -q 'starship init bash | source' "$XDG_CONFIG_HOME/fish/config.fish" 2>/dev/null; then
+    echo 'starship init bash | source' >> "$XDG_CONFIG_HOME/fish/config.fish"
+fi
+
+echo "Starship configuration completed."
